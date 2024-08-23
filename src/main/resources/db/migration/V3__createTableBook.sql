84aab43d97ca5d21ef1ca811d6baba78c6fd05a3
Rui Nascimento <sruy19@gmail.com>
2024-08-23 10:58:55 -0300

    feat: wip api para cadastraqr livros

---
+++ src/main/resources/db/migration/V3__createTableBook.sql
@@ -0,0 +1,19 @@
+CREATE TABLE book
+(
+    id               BIGINT AUTO_INCREMENT PRIMARY KEY,
+    title            VARCHAR(50)  NOT NULL UNIQUE,
+    summary          VARCHAR(500) NOT NULL,
+    table_of_content TEXT         NOT NULL,
+    price            DECIMAL      NOT NULL,
+    number_of_pages  INT          NOT NULL,
+    isbn             VARCHAR(30)  NOT NULL UNIQUE,
+    publication_date DATETIME     NOT NULL,
+    category_id      BIGINT       NOT NULL,
+    author_id        BIGINT       NOT NULL,
+
+    CONSTRAINT fk_book_category
+        FOREIGN KEY (category_id) REFERENCES category (id),
+    CONSTRAINT fk_book_author
+        FOREIGN KEY (author_id) REFERENCES author (id)
+
+);
